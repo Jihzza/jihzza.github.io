87e7bea446c44e26fffcf39f88b00b5b1d8f41c6
Jihzza <rafael.ravalian@gmail.com>
2025-09-13 16:01:47 +0200

    Video FS controls

--- src/pages/sections/OtherWinsSection.jsx
+++ src/pages/sections/OtherWinsSection.jsx
@@ -1,47 +1,48 @@
 // src/pages/sections/OtherWinsSection.jsx
-
-import React from 'react';
-import SectionTextBlack from '../../components/common/SectionTextBlack';
-import ImageCarousel from '../../components/carousel/ImageCarousel';
-import FullScreenVideo from '../../components/video/FullScreenVideo';
-import { useTranslation } from 'react-i18next';
-import TransformationVideo from '../../assets/video/BodyTransformation.mp4';
+import React from "react";
+import SectionTextBlack from "../../components/common/SectionTextBlack";
+import ImageCarousel from "../../components/carousel/ImageCarousel";
+import FullScreenVideo from "../../components/video/FullScreenVideo";
+import { useTranslation } from "react-i18next";
+import TransformationVideo from "../../assets/video/BodyTransformation.mp4";
+// (Optional) import a poster image if you have one
+// import Poster from "../../assets/video/BodyTransformationPoster.jpg";
 
 export default function OtherWinsSection() {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    return (
-        <section className="w-full mx-auto py-4 md:py-4 text-center flex flex-col md:px-6">
-            {/* Title and subtitle section */}
-            <SectionTextBlack title={t('otherWins.title')}>
-                {t('otherWins.bodyTransformation')}
-            </SectionTextBlack>
+  return (
+    <section className="mx-auto flex w-full flex-col text-center md:px-6 py-4 md:py-4">
+      {/* Title + subtitle */}
+      <SectionTextBlack title={t("otherWins.title")}>
+        {t("otherWins.bodyTransformation")}
+      </SectionTextBlack>
 
-            {/* Video section */}
-            <div className="w-full mx-auto">
-                <div className="w-full max-w-md mx-auto justify-center items-center flex">
-                    <FullScreenVideo
-                        src={TransformationVideo}
-                        className="w-45 self-center rounded-xl mt-6 md:w-55 lg:w-45"
-                    />
-                </div>
-            </div>
+      {/* Video */}
+      <div className="w-full mx-auto">
+        <div className="mx-auto flex w-full max-w-md items-center justify-center">
+          <FullScreenVideo
+            src={TransformationVideo}
+            // poster={Poster}
+            autoPlay={false}
+            className="self-center rounded-xl mt-6 w-45 md:w-55 lg:w-45"
+          />
+        </div>
+      </div>
 
-            {/* Social media description section */}
-            <div className="w-full mx-auto">
-                <div className="max-w-4xl mx-auto">
-                    <SectionTextBlack>
-                        {t('otherWins.socialMedia')}
-                    </SectionTextBlack>
-                </div>
-            </div>
+      {/* Social media description */}
+      <div className="w-full mx-auto">
+        <div className="mx-auto max-w-4xl">
+          <SectionTextBlack>{t("otherWins.socialMedia")}</SectionTextBlack>
+        </div>
+      </div>
 
-            {/* Carousel section */}
-            <div className="full-bleed py-8">
-                <div className="w-full mx-auto lg:max-w-6xl desktop-fade-container lg:w-[70%]">
-                    <ImageCarousel />
-                </div>
-            </div>
-        </section>
-    );
+      {/* Carousel */}
+      <div className="full-bleed py-8">
+        <div className="desktop-fade-container mx-auto w-full lg:w-[70%] lg:max-w-6xl">
+          <ImageCarousel />
+        </div>
+      </div>
+    </section>
+  );
 }
